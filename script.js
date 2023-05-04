43ff72a5c28a6cd386bd2ea1295c87b1a7f8bc52
César Manuel Varela García <113544540+cmvgg@users.noreply.github.com>
2023-05-04 21:17:18 +0200

    version 0.4
    version 0.4

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var elem = document.getElementById('cat');
-elem.addEventListener('click', function() {
-    var counter = document.getElementById('counter');
-    var currentValue = parseInt(counter.innerText);
-    counter.innerText = currentValue + 1;
-}, false);
+// Obtener elementos del DOM
+const catImages = document.querySelectorAll('img[data-index]');
+const dogImage = document.getElementById('dog');
+const counter = document.getElementById('counter');
 
-var elem = document.getElementById('cat2');
-elem.addEventListener('click', function() {
-    var counter = document.getElementById('counter');
-    var currentValue = parseInt(counter.innerText);
-    counter.innerText = currentValue + 1;
-}, false);
+// Definir nombres de los gatos
+const catNames = ['Lola', 'Silvestre'];
 
-var elem = document.getElementById('dog');
-elem.addEventListener('click', function() {
-    var counter = document.getElementById('counter');
-    var currentValue = parseInt(counter.innerText);
-    counter.innerText = currentValue - 1;
-}, false) ;
+// Añadir evento de click a las imágenes de los gatos
+catImages.forEach((catImage) => {
+  const catName = catNames[catImage.dataset.index];
+  catImage.addEventListener('click', () => {
+    counter.innerText = parseInt(counter.innerText) + 1;
+    catImage.setAttribute('title', catName);
+  });
+});
 
-
-let catName = "Garfield";
-let catImage = document.getElementById("cat");
-catImage.setAttribute("title", catName);
+// Añadir evento de click al perro
+const dogName = 'Toby';
+dogImage.addEventListener('click', () => {
+  if (parseInt(counter.innerText) > 0) {
+    counter.innerText = parseInt(counter.innerText) - 1;
+    dogImage.setAttribute('title', dogName);
+  }
+});

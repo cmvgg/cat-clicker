52b79b8ede8e952f5618ca7e19e3142bf6811245
César Manuel Varela García <manu220890@gmail.com>
2023-05-04 20:19:55 +0200

    version 0.3

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ elem.addEventListener('click', function() {
     counter.innerText = currentValue + 1;
 }, false);
 
+var elem = document.getElementById('cat2');
+elem.addEventListener('click', function() {
+    var counter = document.getElementById('counter');
+    var currentValue = parseInt(counter.innerText);
+    counter.innerText = currentValue + 1;
+}, false);
+
 var elem = document.getElementById('dog');
 elem.addEventListener('click', function() {
     var counter = document.getElementById('counter');
     var currentValue = parseInt(counter.innerText);
     counter.innerText = currentValue - 1;
 }, false) ;
+
+
+let catName = "Garfield";
+let catImage = document.getElementById("cat");
+catImage.setAttribute("title", catName);
